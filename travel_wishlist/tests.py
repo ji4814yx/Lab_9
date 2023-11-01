cf0d7f4748feb35f9ed169fc505160e7f964cb70
ji4814yx <58522313+ji4814yx@users.noreply.github.com>
2023-11-01 18:44:17 -0500

    django wishlist up to video 9

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
 
 # Create your tests here.
+
+class TestHomePage(TestCase):
+    def test_home_page_shows_empty_list_message_for_empty_database(self):  # use a method
+        home_page_url = reverse('place_list')
+
+
+
